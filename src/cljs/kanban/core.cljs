260de6fc5821a87dbf80dcec51019348ecb03e30
Gaivile <gaivile.info3@gmail.com>
2017-01-21 14:49:14 +0000

    First episode

--- src/cljs/kanban/core.cljs
+++ src/cljs/kanban/core.cljs
@@ -1,11 +1,41 @@
 (ns kanban.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent ]))
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(def app-state
+  (reagent/atom {:columns
+           [{:title "Todos"
+             :cards [{:title "Learn about Reagent"}
+                     {:title "Tell my friends about Lambda Island"
+                      :editing true}]}]}))
 
-(defn greeting []
-  [:h1 (:text @app-state)])
+(defn Card [card]
+  (if (:editing card)
+    [:div.card.editing [:input {:type "text" :value (:title card)}]]
+    [:div.card (:title card)]))
 
-(reagent/render [greeting] (js/document.getElementById "app"))
+(defn NewCard []
+  [:div.new-card
+   "+ add new card"])
+
+(defn Column [{:keys [title cards editing]}]
+  [:div.column
+   (if editing
+     [:input {:type "text" :value title}]
+     [:h2 title])
+   (for [c cards]
+     [Card c])
+   [NewCard]])
+
+(defn NewColumn []
+  [:div.new-column
+   "+ add new column"])
+
+(defn Board [state]
+  [:div.board
+   (for [c (:columns @state)]
+     [Column c])
+   [NewColumn]])
+
+(reagent/render [Board app-state] (js/document.getElementById "app"))
