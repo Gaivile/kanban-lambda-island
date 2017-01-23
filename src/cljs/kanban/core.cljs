3a2747d21522f32ab782fe197c21ec9e85afe2f4
Gaivile <gaivile.info3@gmail.com>
2017-01-23 22:28:35 +0000

    Second episode of Lambda Island on Reagent/Kanban

--- src/cljs/kanban/core.cljs
+++ src/cljs/kanban/core.cljs
@@ -1,41 +1,65 @@
+
 (ns kanban.core
   (:require [reagent.core :as reagent ]))
 
 (enable-console-print!)
 
-(def app-state
+(def board 
   (reagent/atom {:columns
            [{:title "Todos"
              :cards [{:title "Learn about Reagent"}
                      {:title "Tell my friends about Lambda Island"
                       :editing true}]}]}))
 
-(defn Card [card]
-  (if (:editing card)
-    [:div.card.editing [:input {:type "text" :value (:title card)}]]
-    [:div.card (:title card)]))
+(def cards-cursor
+  (reagent/cursor board [:columns 0 :cards]))
+
+(defn- update-title [card-cur title]
+  (swap! card-cur assoc :title title))
+
+(defn- stop-editing [card-cur]
+  (swap! card-cur dissoc :editing))
+
+(defn- start-editing [card-cur]
+  (swap! card-cur assoc :editing true))
+
+(defn- Card [card-cur]
+  (let [{:keys [editing title]} @card-cur]
+    (if editing
+      [:div.card.editing [:input {:type "text"
+                                  :value title
+                                  :autoFocus true
+                                  :on-change #(update-title card-cur (.. % -target -value))
+                                  :on-blur #(stop-editing card-cur)
+                                  :on-key-press #(if (= (.-charCode %) 13)
+                                                   (stop-editing card-cur))}]]
+      [:div.card {:on-click #(start-editing card-cur)} title])))
 
 (defn NewCard []
   [:div.new-card
    "+ add new card"])
 
-(defn Column [{:keys [title cards editing]}]
-  [:div.column
-   (if editing
-     [:input {:type "text" :value title}]
-     [:h2 title])
-   (for [c cards]
-     [Card c])
-   [NewCard]])
+(defn Column [col-cur]
+  (let [{:keys [title cards editing]} @col-cur]
+    [:div.column
+     (if editing
+       [:input {:type "text" :value title}]
+       [:h2 title])
+     (for [i (range (count cards))]
+       [Card (reagent/cursor col-cur [:cards i])])
+     [NewCard]]))
 
 (defn NewColumn []
   [:div.new-column
    "+ add new column"])
 
-(defn Board [state]
+(defn set-title! [board col-idx card-idx title]
+  (swap! board assoc-in [:columns col-idx :cards card-idx :title] title))
+
+(defn Board [board]
   [:div.board
-   (for [c (:columns @state)]
-     [Column c])
+   (for [i (range (count (:columns @board)))]
+     [Column (reagent/cursor board [:columns i])])
    [NewColumn]])
 
-(reagent/render [Board app-state] (js/document.getElementById "app"))
+(reagent/render [Board board] (js/document.getElementById "app"))
